a3fff3d0db2fb55f62a76aa40913ba928b9537dc
rahulshahaha <getrshah@gmail.com>
2021-03-25 19:45:08 -0400

    k

--- app/src/components/itemCards/ItemCardData.js
+++ app/src/components/itemCards/ItemCardData.js
@@ -20,14 +20,14 @@ const projectClick = (e) => {
 
   return ( 
     <div className={'border-2 border-black my-2 w-full self-center grid grid-cols-12 '}>
-      <div onClick={projectClick} className='mainInfo flex flex-col col-span-4 p-2 bg-black text-white'>
+      <div onClick={projectClick} className='hover:bg-opacity-50 mainInfo flex flex-col col-span-4 p-2 bg-black text-white'>
         <p className='text-xs'>{item.score ? item.score.toFixed(2) : 0}</p>
         <p className='text-xl'>{item.project ? item.project.name : ''}</p>
         {/* <p className='text-xs'>{item.importanceName}</p> */}
         <ImportanceIcon importance={item.project ? item.project.importance : 3} />
         <p className='text-sm'>{item.project ? item.project.description : ''}</p>
       </div>
-      <div onClick={itemClick} className={"col-span-8 p-2 " + item.colorClass}>
+      <div onClick={itemClick} className={"hover:bg-opacity-75 col-span-8 p-2 " + item.colorClass}>
         <p className='text-xl'>{item.actionTypeName}</p>
         <p className='text-sm'>{expectedUpdate}</p>
         <p className='text-base'>{item.action}</p>
--- app/src/components/leftBar/ProjectListItem.js
+++ app/src/components/leftBar/ProjectListItem.js
@@ -12,7 +12,7 @@ const ProjectListItem = ({proj, weights}) => {
   }
 
   return ( 
-    <div onClick={click} className="flex justify-between whitespace-nowrap px-2 space-x-2 text-white bg-black rounded-lg cursor-pointer">
+    <div onClick={click} className="transform hover:scale-125 transition ease-out duration-300 flex justify-between whitespace-nowrap px-2 space-x-2 text-white bg-black rounded-lg cursor-pointer">
       <p className="overflow-hidden overflow-ellipsis">{proj.totalScore.toFixed(2)} - {proj.name}</p>
       <div className="self-center">
         <ImportanceIcon importance={proj.importance} importances={weights.importanceArray} />
