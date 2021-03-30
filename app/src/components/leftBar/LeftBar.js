26ee998ede65a24339dba924052ac998388ffd4d
rahulshahaha <getrshah@gmail.com>
2021-03-29 20:00:28 -0400

    logged in and loaded

--- app/src/components/leftBar/LeftBar.js
+++ app/src/components/leftBar/LeftBar.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import AddItem from './AddItem'
 import Login from '../auth/Login'
 import OneOffs from './OneOffs/OneOffs'
 import Config from './todoConfig/Config'
 import TotalScore from './TotalScore'
 import ProjectList from './ProjectList'
+import { FbContext } from '../../store/contexts/fbContext'
+import { DataContext } from '../../store/contexts/dataContext'
 
 
 const LeftBar = () => {
 
   const [showConfig, setShowConfig] = useState(false)
+  const { FBuser } = useContext(FbContext)
+  const { allLoaded } = useContext(DataContext)
 
   const click = (e) => {
     setShowConfig(!showConfig)
@@ -19,16 +23,20 @@ const LeftBar = () => {
     <div className="col-span-3 h-screen overflow-hidden" >
       <div className="h-full overflow-scroll hideBar pb-10">
         <Login />
-        <AddItem />
-        <TotalScore />
-        <OneOffs />
-        <ProjectList />
-        <p className="mt-10 cursor-pointer underline m-auto w-56" onClick={click}>{showConfig ? 'Hide Config' : 'Show Config'}</p>
-        { showConfig ? (
+        { FBuser && allLoaded ? (
           <div>
-            <Config />
+            <AddItem />
+            <TotalScore />
+            <OneOffs />
+            <ProjectList />
+            <p className="mt-10 cursor-pointer underline m-auto w-56" onClick={click}>{showConfig ? 'Hide Config' : 'Show Config'}</p>
+            { showConfig ? (
+              <div>
+                <Config />
+              </div>
+            ) : null}
           </div>
-        ) : null}
+        ): null}
       </div>
     </div>
    );
--- app/src/components/rightBar/RightBar.js
+++ app/src/components/rightBar/RightBar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../../store/contexts/dataContext';
+import { FbContext } from '../../store/contexts/fbContext';
 import { FilterContext } from '../../store/contexts/filterContext';
 import ActionTypeFilter from './ActionTypeFilter';
 import DayFilter from './DayFilter';
@@ -7,8 +8,9 @@ import ImportanceFilters from './ImportanceFilters';
 
 const RightBar = () => {
 
-  const { weights } = useContext(DataContext)
+  const { weights, allLoaded } = useContext(DataContext)
   const { filterDispatch } = useContext(FilterContext)
+  const { FBuser } = useContext(FbContext)
   const importances = weights ? weights.importanceTypes : {}
   const actionTypes = weights ? weights.actionTypes : {}
 
@@ -17,6 +19,10 @@ const RightBar = () => {
     filterDispatch({type: 'ALL_IMPORTANCE', value: true, importances})
     filterDispatch({type: 'ALL_ACTION', value: true, actionTypes})
     filterDispatch({type: 'SET_DAY_FILTER', value: 'all'})
+  }
+
+  if(!FBuser || !allLoaded){
+    return null
   }
 
 
