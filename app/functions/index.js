b8cbeea8f11ea19e11f6c35746482e7d6455ebdd
rahulshahaha <getrshah@gmail.com>
2021-05-06 17:44:40 -0400

    fix function

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const functions = require("firebase-functions");
 const express = require("express");
@@ -11,21 +11,27 @@ const firestore = new Firestore({
 });
 
 
-const oneOffs = express()
-oneOffs.use(cors)
+const oneOffs = express();
+oneOffs.use(cors);
 
 oneOffs.post("/new", (req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  const item = JSON.parse(req.body).item
+  functions.logger.log("body: ", req);
+
+  const body = req.body;
+  const item = body.item;
+
   const now = new Date();
-  res.send(item);
+
 
   firestore.collection("users/f6tnPMjmNnPO6rnutT0EbEMjCOg2/oneOffs").add({
     name: item,
     done: false,
     userID: "f6tnPMjmNnPO6rnutT0EbEMjCOg2",
-    created: now
-  })
+    created: now,
+  }).then(() => {
+    res.send(item);
+  });
 });
 
 exports.oneOffs = functions.https.onRequest(oneOffs);
