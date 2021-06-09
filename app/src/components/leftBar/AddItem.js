88fc47744773efe8128e04e421e143c8b8f5c4cf
rahulshahaha <getrshah@gmail.com>
2021-06-09 14:20:15 -0400

    change add item to dropdown

--- app/src/components/leftBar/AddItem.js
+++ app/src/components/leftBar/AddItem.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { DataContext } from '../../store/contexts/dataContext'
 import { FbContext } from '../../store/contexts/fbContext'
 import { ModalContext } from '../../store/contexts/modalContext'
 
@@ -6,15 +7,36 @@ const AddItem = () => {
 
   const { FBuser } = useContext(FbContext)
   const { modalDispatch } = useContext(ModalContext)
+  const { projects } = useContext(DataContext)
 
-  const addItem = () => {
-    modalDispatch({type:'SHOW_SHEET'})
+  var sortedProjects = []
+  if(projects){
+    sortedProjects  = projects.sort((a,b) => {
+      return b.totalScore - a.totalScore
+    })
   }
+
+
+  // const addItem = () => {
+  //   modalDispatch({type:'SHOW_SHEET'})
+  // }
+
+  const changed = (e) => {
+    console.log(e.target.value)
+    modalDispatch({type: 'SHOW_SHEET', itemProjectID: e.target.value})
+  }
+
 
   if(FBuser === null) return false
 
   return ( 
-    <button className="doneBtn" onClick={addItem}>Add ToDo</button>
+    // <button className="doneBtn" onClick={addItem}>Add ToDo</button>
+    <select autoFocus className='border-2 border-none w-32 bg-doToday rounded-md text-white' id='projectID' onChange={changed} value=''>
+      <option disabled value={''}>Add ToDo</option>
+      { sortedProjects && sortedProjects.map(project => {
+        return <option key={project.id} value={project.id}>{project.name}</option> 
+      })}
+    </select>
    );
 }
  
