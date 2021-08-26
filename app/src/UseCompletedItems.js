af13262b9fe26b45461434889d8c741679b26910
rahulshahaha <getrshah@gmail.com>
2021-08-26 14:41:17 -0400

    Update UseCompletedItems.js

--- app/src/UseCompletedItems.js
+++ app/src/UseCompletedItems.js
@@ -1,19 +1,24 @@
 import { useContext, useState, useEffect } from 'react';
 import { CompletedItemsContext } from './store/contexts/completedItemsContext';
 import  moment from 'moment'
+import { DataContext } from './store/contexts/dataContext';
 
 
 export default function useHistory(projectID) {
 
   const [items, setItems] = useState([])
   const { completedItems } = useContext(CompletedItemsContext)
+  const { weights } = useContext(DataContext)
 
 
   useEffect(() => {
     const newItems = completedItems.filter(item => {
       return item.projectID === projectID
     }).map(item => {
-      return {...item, score:item.scoreOnComplete, expectedUpdate: item.completedDate}
+      const actionType = weights.actionTypeArray.filter(aType => {
+        return aType.id === item.actionType
+      })[0]
+      return {...item, score:item.scoreOnComplete, expectedUpdate: item.completedDate, actionTypeName: actionType.name, colorClass: 'bg-neverDo'}
     }).sort((a,b) => {
       if(moment.unix(a.completedDate.seconds).isBefore(moment.unix(b.completedDate.seconds))){
         return 1
@@ -23,7 +28,7 @@ export default function useHistory(projectID) {
       return 0
     }).slice(0, 5)
     setItems(newItems)
-  }, [completedItems,projectID])
+  }, [completedItems,projectID, weights.actionTypeArray])
 
 
   return items;
